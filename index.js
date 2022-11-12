6418c2b070c965e556262c1dd1be2da6e8b7f584
ViktoriaSysun <vikasysun18@gmail.com>
2022-11-12 21:01:55 +0200

    commit hw1

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
 const fs = require('node:fs/promises');
 const path = require('path');
+//
+// const foo = async () => {
+//  const folderPath = path.join(__dirname,'boys');
+//     // console.log(folderPath);
+//
+// const files = await fs.readdir(folderPath);
+//     // console.log(files);
+//
+//     for (const file of files) {
+//         const filePath = path.join(folderPath,file);
+//       const data = await fs.readFile(filePath);
+//         // console.log(data.toString());
+//         const user = JSON.parse(data);
+//
+//         if (user.gender === 'female') {
+//             await fs.rename(filePath,path.join(__dirname, 'girls', file))
+//         }
+//     }
+// }
+//
+// const foo1 = async () => {
+//     const folderPath = path.join(__dirname,'girls');
+//     // console.log(folderPath);
+//
+//     const files = await fs.readdir(folderPath);
+//     // console.log(files);
+//
+//     for (const file of files) {
+//         const filePath = path.join(folderPath,file);
+//         const data = await fs.readFile(filePath);
+//         // console.log(data.toString());
+//         const user = JSON.parse(data);
+//
+//         if (user.gender === 'male') {
+//             await fs.rename(filePath,path.join(__dirname, 'boys', file))
+//         }
+//     }
+// }
+//
+// foo()
+// foo1()
 
-const foo = async () => {
- const folderPath = path.join(__dirname,'boys');
-    // console.log(folderPath);
+const sorter = async (readFolder,writeFolder,gender) => {
+    const folderPath = path.join(__dirname,readFolder);
 
-const files = await fs.readdir(folderPath);
-    // console.log(files);
+    const files = await fs.readdir(folderPath);
+
     for (const file of files) {
-      const data = await fs.readFile(path.join(folderPath,file));
-        console.log(data.toString());
+        const filePath = path.join(folderPath,file);
+        const data = await fs.readFile(filePath);
+        const user = JSON.parse(data);
+
+        if (user.gender ===gender ) {
+            await fs.rename(filePath,path.join(__dirname, writeFolder, file))
+        }
     }
 }
 
-foo()
+sorter('boys','girls','female');
+sorter('girls','boys','male');
